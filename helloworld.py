791532ffc0bceea207697c3b5a6a9d32b942bb11
glvx <guillaume.liverneaux.lbs@gmail.com>
2021-11-04 16:26:20 +0100

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,8 +1,13 @@
 import sys
-def main(arg1):
+import os
+
+def main(arg1="pasbien"):
     print("Hello World!"+arg1)
+    os.system('ls -l'++arg1)
+    secret="1234"
+    password="12345"
 
 if __name__ == "__main__":
-    main(name = sys.argv[1])
+    main(sys.argv[1])
 
 print("Guru99")
